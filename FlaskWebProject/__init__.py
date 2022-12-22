b53e2518a2426e6d15b75666eabe2414971eec88
Phúc Thịnh <phucthinhbach@gmail.com>
2022-12-22 21:55:55 +0700

    commit 3

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,7 +11,10 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-app.logger.info("Article CMS")
+app.logger.setLevel(logging.ERROR)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.ERROR)
+app.logger.addHandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
